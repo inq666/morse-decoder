6af524b8c9e0cc60b98282df4169ee5b8a7fd94e
Yahor Neverouski <hardstyle6537@gmail.com>
2020-03-03 18:06:56 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -35,11 +35,39 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
-};
+}
 
 function decode(expr) {
-    // write your solution here
+
+    let phrase = [];
+
+    for (let j = 0; j < expr.length;) {
+        exprStr = expr.slice(j, j + 10);
+
+        newExpr = exprStr
+            .replace(/\*/g, 'q')
+            .replace(/11/g, '-')
+            .replace(/10/g, '.')
+            .replace(/00/g, ' ')
+            .replace(/qqqqqqqqqq/g, ' space')
+            .replace(/ +/g, ' ').trim()
+
+        exprArr = newExpr.split(" ")
+
+        for (let i = 0; i < exprArr.length; i++) {
+            if (exprArr[i] == "space") {
+                phrase.push(" ");
+            } else {
+                let key = exprArr[i];
+
+                phrase.push(MORSE_TABLE[key])
+            }
+        }
+        j += 10;
+    }
+    return phrase.join('');
 }
+
 
 module.exports = {
     decode
